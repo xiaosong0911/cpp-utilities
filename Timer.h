e0920ae4b386401be04ba1821af710b844c0ea80
Xiaosong Chen <xiaosong0911@gmail.com>
2019-03-11 17:58:24 +0800

    add macro for scope timing

--- Timer.h
+++ Timer.h
@@ -26,47 +26,20 @@ private:
     time_point starting_point_;
 };
 
+#ifndef infofln
+#define infofln(fmt, ...) fprintf(stderr, fmt"\n", __VA_ARGS__)
+#endif infofln
 
-#include <string>
-#include <vector>
-#include <algorithm>
-
-class TimerManager {
-public:
-    std::vector<std::string> name;
-    std::vector<std::vector<double>> record;
-    void recordTime(std::string n, double r) {
-        int i = std::find(name.begin(), name.end(), n) - name.begin();
-        record[i].push_back(r);
+struct scoped_timer_logger {
+    const char * tag;
+    Timer t;
+    scoped_timer_logger(const char * tag_) {
+        tag = tag_;
+        t.reset();
     }
-    struct scoped_timer {
-        scoped_timer(TimerManager & m, std::string n) : owner(m), name(n) {}
-        scoped_timer(scoped_timer &&) = default;
-        scoped_timer& operator=(scoped_timer &&) = default;
-        scoped_timer(const scoped_timer &) = delete;
-        scoped_timer& operator=(const scoped_timer &) = delete;
-        ~scoped_timer() {
-            owner.recordTime(name, t.duration_in_seconds());
-        }
-        TimerManager & owner;
-        std::string name;
-        Timer t;
-    };
-public:
-    scoped_timer scope_timer(std::string n) {
-        if (std::find(name.begin(), name.end(), n) == name.end()) {
-            name.push_back(n);
-            record.push_back({});
-        }
-        return scoped_timer(*this, n);
-    }
-    void print(FILE * fp) {
-        for (int i = 0; i < name.size(); i ++) {
-            fprintf(fp, "%s", name[i].c_str());
-            for (auto t : record[i]) {
-                fprintf(fp, " %lg", t);
-            }
-            fprintf(fp, "\n");
-        }
+    ~scoped_timer_logger() {
+        infofln("[timing] %s for %lg ms", tag, t.duration_in_seconds() * 100);
     }
 };
+
+#define TIME_SCOPE(tag) scoped_timer_logger __l##__LINE__ (tag);
